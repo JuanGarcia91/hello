a4922c2bddf9ba05eee2e67cdb8eff3802dc52c1
Juan Garcia <jgarcia50@huskers.unl.edu>
2022-08-25 14:51:45 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Juan Garcia, Computer Engineering\n");
+  printf("The School of Computing Rocks!\n");
+  return 0;
+}
