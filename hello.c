4c0f389854ed9f9749a44105f7bbd97c0a561a3b
Juan Garcia <jgarcia50@huskers.unl.edu>
2022-08-25 16:24:01 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
 
   printf("Juan Garcia, Computer Engineering\n");
   printf("The School of Computing Rocks!\n");
+  printf ("Minor is Japanese\n");
 
   return 0;
 }
