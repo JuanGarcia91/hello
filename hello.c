64877c8914d7aeaebfb74a1b9c2df14eda580f41
sdubois3 <111895402+sdubois3@users.noreply.github.com>
2022-08-29 16:12:30 +0000

    Sam added his name and major

--- hello.c
+++ hello.c
@@ -2,11 +2,16 @@
  * Basic Hello World Program
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 
   printf("Juan Garcia, Computer Engineering\n");
   printf("The School of Computing Rocks!\n");
   printf ("Minor is Japanese\n");
+  printf("Hello World, I'm Sam DuBois!\n");
+  printf("My major is... Computer Science!");
 
   return 0;
 }
